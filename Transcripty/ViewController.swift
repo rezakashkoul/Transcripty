42a32e8d4ea3db904cab5ef15198ab4f7e870504
Reza Kashkoul <rezakashkoul@gmail.com>
2021-09-26 19:46:09 +0330

    Initial Commit

---
+++ Transcripty/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Transcripty
+//
+//  Created by Reza Kashkoul on 7/4/1400 AP.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
